b37258d44b7ecbb6333f2cd1faab7be022b8c8bd
Daniel Moreira <dmcardoso.99@hotmail.com>
2019-10-11 10:10:14 -0300

    :rocket: Add flameshot and include it as a custom screenshot utility

--- ubuntu/installer.sh
+++ ubuntu/installer.sh
@@ -79,8 +79,18 @@ apt install qbittorrent -y
 ## Editor de texto Vim
 apt install vim -y
 
-## Deepin Screenshot
-apt install deepin-screenshot -y
+## Flameshot Screenshot
+apt install flameshot -y
+## Release printscree key
+gsettings set org.gnome.settings-daemon.plugins.media-keys screenshot ''
+## Set new custom binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+## Set name
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'flameshot'
+## Set command
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command '/usr/bin/flameshot gui'
+## Set binding
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding 'Print'
 
 # Desenvolvimento
 
