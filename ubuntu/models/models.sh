d3aff8c7b97f0384794d697d1c72f45971156ead
Daniel Moreira <dmcardoso.99@hotmail.com>
2019-10-22 10:00:00 -0300

    :rocket: Fix models folder

--- ubuntu/models/models.sh
+++ ubuntu/models/models.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname "$0")
 
 # Copiar Modelos
 cp -r ${BASEDIR}/assets /home/$USER/Modelos
-chown -R ${SUDO_USER}:${SUDO_USER} ~/Modelos
+chown -R ${SUDO_USER}:${SUDO_USER} /home/$USER/Modelos
