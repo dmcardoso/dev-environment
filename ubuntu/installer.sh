47cc61f9d06eff7f747f5ec9e26ff61f3611f73f
Daniel Moreira <dmcardoso.99@hotmail.com>
2019-12-14 10:59:26 -0300

    :rocket: Adiciona mongodb compass

--- ubuntu/installer.sh
+++ ubuntu/installer.sh
@@ -119,6 +119,13 @@ git config --global credential.helper store
 ## Filezilla
 apt install filezilla -y
 
+## Mongodb compass
+wget https://downloads.mongodb.com/compass/mongodb-compass-community_1.20.2_amd64.deb
+apt --fix-broken install
+apt install libgconf-2-4 -y
+dpkg -i mongodb-compass-community_1.20.2_amd64.deb
+rm -f mongodb-compass-community_1.20.2_amd64.deb
+
 ## Navicat
 bash ${BASEDIR}/apps/navicat/navicat.sh
 
