49e880726c9d2287f87a5f0f56a2233c31412f2b
Daniel Moreira <dmcardoso.99@hotmail.com>
2020-02-01 14:30:25 -0300

    :rocket: Update docker installation

--- ubuntu/installer.sh
+++ ubuntu/installer.sh
@@ -185,7 +185,7 @@ chsh -s $(which zsh)
 if [[ "$docker" == "y" ]]; then
   ## Docker
   apt install docker.io -y
-  curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
   systemctl enable docker
   groupadd docker
